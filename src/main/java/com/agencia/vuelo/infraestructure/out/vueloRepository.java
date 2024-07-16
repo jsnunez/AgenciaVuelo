a1e8ec55b9150f5e64b6de640ad2a242fc104e4f
joan sebastian nuñez serrano <jsnunez94@gmail.com>
2024-07-16 14:31:30 -0500

    agregar clientes

--- src/main/java/com/agencia/vuelo/infraestructure/out/vueloRepository.java
+++ src/main/java/com/agencia/vuelo/infraestructure/out/vueloRepository.java
@@ -11,16 +11,13 @@ import java.util.Date;
 import java.util.List;
 import java.util.Properties;
 
-import javax.swing.JComboBox;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
 import javax.swing.*;
 import java.awt.*;
 import java.math.BigDecimal;
 
 import com.agencia.vuelo.domain.entity.BuscarVuelo;
 import com.agencia.vuelo.domain.entity.Ciudad;
+import com.agencia.vuelo.domain.entity.Pasajero;
 import com.agencia.vuelo.domain.entity.VuelosDto;
 import com.agencia.vuelo.domain.entity.vuelo;
 import com.agencia.vuelo.domain.service.vueloService;
@@ -52,8 +49,8 @@ public class vueloRepository implements vueloService {
   @Override
   public vuelo findvuelo(int id) {
     return null;
-   
-    }
+
+  }
 
   @Override
   public void updatevuelo(vuelo vuelo) {
@@ -75,10 +72,11 @@ public class vueloRepository implements vueloService {
 
   @Override
   public VuelosDto consultvuelo(int id) {
-    
+
     VuelosDto vuelo = new VuelosDto();
     try {
-      // String sql = "SELECT id, fechaviaje, precioviaje,idorigenaeropuerto,iddestinoaeropuerto FROM viajes WHERE id = ?";
+      // String sql = "SELECT id, fechaviaje,
+      // precioviaje,idorigenaeropuerto,iddestinoaeropuerto FROM viajes WHERE id = ?";
       String sql = "SELECT  v.id,v.precioviaje,v.fechaviaje ,a.nombre AS aeropuertoorigen,c.nombre AS ciudadorigen ,ad.nombre AS aeropuertodestino, cd.nombre AS ciudaddestino from viajes v       Join aeropuertos a       on a.id = v.idorigenaeropuerto       JOIN ciudades c       on a.idciudad =c.id      Join aeropuertos ad       on ad.id = v.iddestinoaeropuerto        JOIN ciudades cd       on ad.idciudad =cd.id      WHERE  v.id=?;";
       PreparedStatement statement = connection.prepareStatement(sql);
       statement.setLong(1, id);
@@ -89,16 +87,16 @@ public class vueloRepository implements vueloService {
           vuelo.setPrecioviaje((resultSet.getBigDecimal("precioviaje")));
           vuelo.setAeropuertoOrigen((resultSet.getString("aeropuertoorigen")));
           vuelo.setCiudadOrigen((resultSet.getString("ciudadorigen")));
-          vuelo.setAeropuertoDestion((resultSet.getString("aeropuertodestino"))); 
+          vuelo.setAeropuertoDestion((resultSet.getString("aeropuertodestino")));
           vuelo.setCiudadDestino((resultSet.getString("ciudaddestino")));
         }
-JOptionPane.showMessageDialog(null, "vuelo encontrada: \n" +
-                        "Fecha: " + vuelo.getFechaviaje() + "\n" +
-                        "Precio: " + vuelo.getPrecioviaje() + "\n" +
-                        "Aeropuerto Origen: " +vuelo.getAeropuertoOrigen() + "\n" +
-                        "Ciudad: " + vuelo.getCiudadOrigen() + "\n"+
-                        "Aeropuerto Destino: " +vuelo.getCiudadDestino() + "\n" +
-                        "Ciudad: " + vuelo.getCiudadDestino());
+        JOptionPane.showMessageDialog(null, "vuelo encontrada: \n" +
+            "Fecha: " + vuelo.getFechaviaje() + "\n" +
+            "Precio: " + vuelo.getPrecioviaje() + "\n" +
+            "Aeropuerto Origen: " + vuelo.getAeropuertoOrigen() + "\n" +
+            "Ciudad: " + vuelo.getCiudadOrigen() + "\n" +
+            "Aeropuerto Destino: " + vuelo.getCiudadDestino() + "\n" +
+            "Ciudad: " + vuelo.getCiudadDestino());
 
       } catch (SQLException e) {
         e.printStackTrace();
@@ -112,29 +110,28 @@ JOptionPane.showMessageDialog(null, "vuelo encontrada: \n" +
 
   @Override
   public void deletevuelo(int id) {
-    try{
-    String sql = "DELETE FROM vuelosvuelos WHERE id = ?";
-        int rowsUpdate = 0;
-    
-             PreparedStatement statement = connection.prepareStatement(sql) ;
-    
-            statement.setInt(1, id);
-            rowsUpdate = statement.executeUpdate();
-    
-            System.out.println("Número de filas eliminadas: " + rowsUpdate);
-    
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        
+    try {
+      String sql = "DELETE FROM vuelosvuelos WHERE id = ?";
+      int rowsUpdate = 0;
 
-}
+      PreparedStatement statement = connection.prepareStatement(sql);
+
+      statement.setInt(1, id);
+      rowsUpdate = statement.executeUpdate();
+
+      System.out.println("Número de filas eliminadas: " + rowsUpdate);
+
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+
+  }
 
   @Override
   public void BuscarVuelo(List<Ciudad> ciudades) {
     List<String> nombres = new ArrayList<>();
     for (Ciudad ciudad : ciudades) {
-        nombres.add(ciudad.getCiudad());
+      nombres.add(ciudad.getCiudad());
     }
 
     JComboBox<String> comboBoxCiudadesorigen = new JComboBox<>(nombres.toArray(new String[0]));
@@ -146,132 +143,216 @@ JOptionPane.showMessageDialog(null, "vuelo encontrada: \n" +
     panel.add(new JLabel("Seleccione una ciudad destino:"));
     panel.add(comboBoxCiudadesdestino);
 
-    int result = JOptionPane.showConfirmDialog(null, panel, "Seleccionar Ciudad origen y destino", JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
+    int result = JOptionPane.showConfirmDialog(null, panel, "Seleccionar Ciudad origen y destino",
+        JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
     if (result == JOptionPane.OK_OPTION) {
-        String selectedCiudadorigen = (String) comboBoxCiudadesorigen.getSelectedItem();
-        String selectedCiudaddestino = (String) comboBoxCiudadesdestino.getSelectedItem();
-        System.out.println("Ciudad origen seleccionada: " + selectedCiudadorigen);
-        System.out.println("Ciudad destino seleccionada: " + selectedCiudaddestino);
+      String selectedCiudadorigen = (String) comboBoxCiudadesorigen.getSelectedItem();
+      String selectedCiudaddestino = (String) comboBoxCiudadesdestino.getSelectedItem();
+      System.out.println("Ciudad origen seleccionada: " + selectedCiudadorigen);
+      System.out.println("Ciudad destino seleccionada: " + selectedCiudaddestino);
 
-        JCalendar calendar = new JCalendar();
-        JPanel panelc = new JPanel(new GridLayout(0, 2));
-        panelc.add(new JLabel("Seleccione una fecha:"));
-        panelc.add(calendar);
+      JCalendar calendar = new JCalendar();
+      JPanel panelc = new JPanel(new GridLayout(0, 2));
+      panelc.add(new JLabel("Seleccione una fecha:"));
+      panelc.add(calendar);
 
-        int resultc = JOptionPane.showConfirmDialog(null, panelc, "Seleccionar Fecha", JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
-        if (resultc == JOptionPane.OK_OPTION) {
-            Date selectedDate = calendar.getDate();
-            Calendar cal = Calendar.getInstance();
-            cal.setTime(selectedDate);
-            int dia = cal.get(Calendar.DAY_OF_MONTH);
-            int mes = cal.get(Calendar.MONTH) + 1; // Los meses comienzan en 0 en Calendar, por lo que se añade 1
-            int año = cal.get(Calendar.YEAR);
-            String fechaida= año+"-"+mes+"-"+dia;
-            System.out.println(fechaida);
+      int resultc = JOptionPane.showConfirmDialog(null, panelc, "Seleccionar Fecha", JOptionPane.OK_CANCEL_OPTION,
+          JOptionPane.PLAIN_MESSAGE);
+      if (resultc == JOptionPane.OK_OPTION) {
+        Date selectedDate = calendar.getDate();
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(selectedDate);
+        int dia = cal.get(Calendar.DAY_OF_MONTH);
+        int mes = cal.get(Calendar.MONTH) + 1; // Los meses comienzan en 0 en Calendar, por lo que se añade 1
+        int año = cal.get(Calendar.YEAR);
+        String fechaida = año + "-" + mes + "-" + dia;
+        System.out.println(fechaida);
 
-            String idAeropuertoOrigen = obtenerIdAeropuerto(selectedCiudadorigen);
-            String idAeropuertoDestino = obtenerIdAeropuerto(selectedCiudaddestino);
+        String idAeropuertoOrigen = obtenerIdAeropuerto(selectedCiudadorigen);
+        String idAeropuertoDestino = obtenerIdAeropuerto(selectedCiudaddestino);
 
-            System.out.println("ID Aeropuerto Origen: " + idAeropuertoOrigen);
-            System.out.println("ID Aeropuerto Destino: " + idAeropuertoDestino);
+        System.out.println("ID Aeropuerto Origen: " + idAeropuertoOrigen);
+        System.out.println("ID Aeropuerto Destino: " + idAeropuertoDestino);
 
-BuscarVuelo bvuelo= new BuscarVuelo(fechaida,idAeropuertoOrigen,idAeropuertoDestino);
-vuelofecha(bvuelo);
+        BuscarVuelo bvuelo = new BuscarVuelo(fechaida, idAeropuertoOrigen, idAeropuertoDestino);
+        vuelofecha(bvuelo);
 
-        } else {
-            System.out.println("Selección de fecha cancelada");
-        }
-    } else {
-        System.out.println("Selección de ciudades cancelada");
-    }
-}
-  private void vuelofecha(BuscarVuelo bvuelo) {
-     System.out.println(bvuelo.getIdAeropuertoDestino());
-     System.out.println(bvuelo.getIdAeropuertoOrigen());
-     List<String> vuelos = new ArrayList<>();
-      String sql = "SELECT id,precioviaje,fechaviaje from viajes v WHERE fechaviaje = ? AND  idorigenaeropuerto=? AND  iddestinoaeropuerto =?";
-   
-        
-          try (PreparedStatement statement = connection.prepareStatement(sql)) {
-            statement.setString(1, bvuelo.getFechaIda());
-            statement.setString(2, bvuelo.getIdAeropuertoOrigen());
-            statement.setString(3, bvuelo.getIdAeropuertoDestino());
-            try (ResultSet resultSet = statement.executeQuery()) {
-                while (resultSet.next()) {
-                  vuelos.add(resultSet.getString("id"));
-                  
-                 
-                }
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-            System.out.println(vuelos);
-            JComboBox<String> comboBoxVuelos = new JComboBox<>(vuelos.toArray(new String[0]));
-
-            JPanel panel = new JPanel(new GridLayout(0, 2));
-            panel.add(new JLabel("Seleccione Vuelo:"));
-            panel.add(comboBoxVuelos);
-
-            int result = JOptionPane.showConfirmDialog(null, panel, "Seleccionar vuelo", JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
-          // try (ResultSet resultSet = statement.executeQuery()) {
-          //     if (resultSet.next()) {
-          //        int id = resultSet.getInt("id");
-          //        BigDecimal precio = resultSet.getBigDecimal("precioviaje");
-          //        JOptionPane.showMessageDialog(null, "vuelo encontrada:" +id +" \n" +
-          //        "Fecha: " + bvuelo.getFechaIda() + "\n" +
-          //        "Precio: " + precio + "\n" +
-          //        "Aeropuerto Origen: " +bvuelo.getIdAeropuertoOrigen() + "\n" +
-          //        "Aeropuerto Destino: " +bvuelo.getIdAeropuertoDestino() + "\n" 
-          //        );
-                 
-          //     }else{
-          //        JOptionPane.showMessageDialog(null, " no se encontro vuelo" );
-          //     }
-          // }
-          
-      } catch (SQLException e) {
-          e.printStackTrace();
+      } else {
+        System.out.println("Selección de fecha cancelada");
       }
-     
+    } else {
+      System.out.println("Selección de ciudades cancelada");
+    }
   }
 
-    private String obtenerIdAeropuerto(String nombreCiudad) {
-      String idAeropuerto = null;
-      String sql = "SELECT a.id FROM aeropuertos a JOIN ciudades c ON c.id = a.idciudad WHERE c.nombre = ?";
-      try (PreparedStatement statement = connection.prepareStatement(sql)) {
-          statement.setString(1, nombreCiudad);
-          try (ResultSet resultSet = statement.executeQuery()) {
-              if (resultSet.next()) {
-                  idAeropuerto = resultSet.getString("id");
-              }
-          }
+  private void vuelofecha(BuscarVuelo bvuelo) {
+    System.out.println(bvuelo.getIdAeropuertoDestino());
+    System.out.println(bvuelo.getIdAeropuertoOrigen());
+    List<String> vuelos = new ArrayList<>();
+    String sql = "SELECT id,precioviaje,fechaviaje from viajes v WHERE fechaviaje = ? AND  idorigenaeropuerto=? AND  iddestinoaeropuerto =?";
+
+    try (PreparedStatement statement = connection.prepareStatement(sql)) {
+      statement.setString(1, bvuelo.getFechaIda());
+      statement.setString(2, bvuelo.getIdAeropuertoOrigen());
+      statement.setString(3, bvuelo.getIdAeropuertoDestino());
+      try (ResultSet resultSet = statement.executeQuery()) {
+        while (resultSet.next()) {
+          vuelos.add(resultSet.getString("id"));
+
+        }
       } catch (SQLException e) {
-          e.printStackTrace();
+        e.printStackTrace();
       }
-      return idAeropuerto;
+      System.out.println(vuelos);
+      JComboBox<String> comboBoxVuelos = new JComboBox<>(vuelos.toArray(new String[0]));
+
+      JPanel panel = new JPanel(new GridLayout(0, 2));
+      panel.add(new JLabel("Seleccione Vuelo:"));
+      panel.add(comboBoxVuelos);
+
+      int resultVuelo = JOptionPane.showConfirmDialog(null, panel, "Seleccionar vuelo", JOptionPane.OK_CANCEL_OPTION,
+          JOptionPane.PLAIN_MESSAGE);
+          String selectVuelo;
+          if (resultVuelo == JOptionPane.OK_OPTION) {
+             selectVuelo = (String) comboBoxVuelos.getSelectedItem();
+             System.out.println(selectVuelo);
+          }
+        
+   
+      var yesOrNo = 0;
+
+      while (yesOrNo == 0) {
+
+        agregarPasajero();
+        yesOrNo = JOptionPane.showConfirmDialog(null, "Desea agregar un nuevo pasajero?");
+
+      }
+      if (yesOrNo == 1) { 
+        JOptionPane.showMessageDialog(null, "Selecciona silla");
+      }
+
+      try {
+        String query = "INSERT INTO reservaviaje (fecha,idvuelos,idclientes,estado) VALUES (?,?,?,?)";
+        PreparedStatement ps = connection.prepareStatement(query,
+            PreparedStatement.RETURN_GENERATED_KEYS);
+        ps.setString(1,  bvuelo.getFechaIda());
+        ps.setInt(2, 1);
+        ps.setInt(3,1);
+        ps.setString(4,"reservado");
+
+        ps.executeUpdate();
+      } catch (SQLException e) {
+        e.printStackTrace();
+  
+      }
+
+      // try (ResultSet resultSet = statement.executeQuery()) {
+      // if (resultSet.next()) {
+      // int id = resultSet.getInt("id");
+      // BigDecimal precio = resultSet.getBigDecimal("precioviaje");
+      // JOptionPane.showMessageDialog(null, "vuelo encontrada:" +id +" \n" +
+      // "Fecha: " + bvuelo.getFechaIda() + "\n" +
+      // "Precio: " + precio + "\n" +
+      // "Aeropuerto Origen: " +bvuelo.getIdAeropuertoOrigen() + "\n" +
+      // "Aeropuerto Destino: " +bvuelo.getIdAeropuertoDestino() + "\n"
+      // );
+
+      // }else{
+      // JOptionPane.showMessageDialog(null, " no se encontro vuelo" );
+      // }
+      // }
+
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+
+  }
+
+  private void agregarPasajero() {
+    JPanel panel = new JPanel(new GridLayout(0, 2));
+
+    JLabel nombreLabel = new JLabel("Nombre:");
+    JTextField nombreField = new JTextField();
+    panel.add(nombreLabel);
+    panel.add(nombreField);
+    JLabel edadLabel = new JLabel("edad:");
+    JTextField edadField = new JTextField();
+    panel.add(edadLabel);
+    panel.add(edadField);
+    
+    List<String> listTiposDocuemtnos = new ArrayList<>();
+    String sql = "SELECT nombre FROM tiposdocumentos";
+
+    try (PreparedStatement statement = connection.prepareStatement(sql)) {
+      try (ResultSet resultSet = statement.executeQuery()) {
+        while (resultSet.next()) {
+          listTiposDocuemtnos.add(resultSet.getString("nombre"));
+
+        }
+      } catch (SQLException e) {
+        e.printStackTrace();
+      }
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+
+    JComboBox<String> comboBoxTipoDocumento = new JComboBox<>(listTiposDocuemtnos.toArray(new String[0]));
+
+    // JPanel panel = new JPanel(new GridLayout(0, 2));
+    panel.add(new JLabel("Seleccione tipo Documento:"));
+    panel.add(comboBoxTipoDocumento);
+    JLabel documetoJLabel = new JLabel("Numero documento:");
+    JTextField documentoField = new JTextField();
+    panel.add(documetoJLabel);
+    panel.add(documentoField);
+    int result = JOptionPane.showConfirmDialog(null, panel, "Seleccionar tipo Documento", JOptionPane.OK_CANCEL_OPTION,
+        JOptionPane.PLAIN_MESSAGE);
+        String selectedTipoDocumento="";
+    int edadInt = Integer.parseInt(edadField.getText());
+    if (result == JOptionPane.OK_OPTION) {
+      selectedTipoDocumento = (String) comboBoxTipoDocumento.getSelectedItem();
+    }
+    Pasajero pasajero = new Pasajero(nombreField.getText(), edadInt, selectedTipoDocumento, documentoField.getText());
+
+  }
+
+  private String obtenerIdAeropuerto(String nombreCiudad) {
+    String idAeropuerto = null;
+    String sql = "SELECT a.id FROM aeropuertos a JOIN ciudades c ON c.id = a.idciudad WHERE c.nombre = ?";
+    try (PreparedStatement statement = connection.prepareStatement(sql)) {
+      statement.setString(1, nombreCiudad);
+      try (ResultSet resultSet = statement.executeQuery()) {
+        if (resultSet.next()) {
+          idAeropuerto = resultSet.getString("id");
+        }
+      }
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+    return idAeropuerto;
   }
 
   @Override
   public List<Ciudad> findAllCiudades() {
     List<Ciudad> ciudades = new ArrayList<>();
     String sql = "SELECT id, nombre FROM ciudades";
-    
-    try (PreparedStatement statement = connection.prepareStatement(sql)) {
-        try (ResultSet resultSet = statement.executeQuery()) {
-            while (resultSet.next()) {
-                Ciudad ciudad = new Ciudad();
-                ciudad.setId(resultSet.getString("id"));
-                ciudad.setCiudad(resultSet.getString("nombre"));
-                ciudades.add(ciudad);
-                System.out.println(ciudad.getCiudad());
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    } catch (Exception e) {
-        e.printStackTrace();
-    }
-    
-    return ciudades;
-}  }
 
+    try (PreparedStatement statement = connection.prepareStatement(sql)) {
+      try (ResultSet resultSet = statement.executeQuery()) {
+        while (resultSet.next()) {
+          Ciudad ciudad = new Ciudad();
+          ciudad.setId(resultSet.getString("id"));
+          ciudad.setCiudad(resultSet.getString("nombre"));
+          ciudades.add(ciudad);
+          System.out.println(ciudad.getCiudad());
+        }
+      } catch (SQLException e) {
+        e.printStackTrace();
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
+    return ciudades;
+  }
+}
