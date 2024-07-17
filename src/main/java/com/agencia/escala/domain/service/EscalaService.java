91c854c0873583bf8f86e360aaab416b30c6ca1d
silviaac28 <silviaangarita28@gmail.com>
2024-07-17 14:59:50 -0500

    escalas LISTA

--- src/main/java/com/agencia/escala/domain/service/EscalaService.java
+++ src/main/java/com/agencia/escala/domain/service/EscalaService.java
@@ -1,10 +1,12 @@
 package com.agencia.escala.domain.service;
 
+import java.util.List;
+
 import com.agencia.escala.domain.entity.Escala;
 
 public interface EscalaService {
 
-    Escala findEscala(int idViaje);
+    List<Escala> findEscala(int idViaje);
     void updateEscala(Escala escala);
     Escala deleteEscala(int id);
 
